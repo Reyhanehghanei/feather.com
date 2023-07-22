08f6bb3ec52efa162653a8921c3e628c36c3bbf2
Reyhanehghanei <regh9108@gmail.com>
2023-07-22 06:19:24 -0500

    new

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -7,29 +7,13 @@ function show() {
     }
 }
 
-<script>
-    $(function() {
-        // OPACITY OF BUTTON SET TO 0%
-        $(".roll").css("opacity", "0");
 
-    // ON MOUSE OVER
-    $(".roll").hover(function () {
 
-        // SET OPACITY TO 70%
-        $(this).stop().animate({
-            opacity: .7
-        }, "slow");
-            },
-
-    // ON MOUSE OUT
-    function () {
-
-        // SET OPACITY BACK TO 50%
-        $(this).stop().animate({
-            opacity: 0
-        }, "slow");
-            });
+$(document).ready(function () {
+    $(".hover-out").mouseover(function () {
+        $(".hover-out").css("transform", "translateY(-20px)")
     });
-</script>
-
-
+    $(".hover-out").mouseout(function () {
+        $(".hover-out").css("transform", "translateY(-20px)")
+    })
+})
