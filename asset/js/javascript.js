a95a80da2d8e60381bd944c332bcd0fe3a87416e
Reyhanehghanei <regh9108@gmail.com>
2023-07-22 19:26:47 -0500

    new

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -7,13 +7,3 @@ function show() {
     }
 }
 
-
-
-$(document).ready(function () {
-    $(".hover-out").mouseover(function () {
-        $(".hover-out").css("transform", "translateY(-20px)")
-    });
-    $(".hover-out").mouseout(function () {
-        $(".hover-out").css("transform", "translateY(-20px)")
-    })
-})
